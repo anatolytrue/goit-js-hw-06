02f377a8f33a785a48eb55639e01916e3441e3da
Anatolii <anatoly.tomnyak@gmail.com>
2022-07-10 18:17:02 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listIngredients = document.querySelector('#ingredients');
+const list = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  const listIngredients = document.querySelector('#ingredients');
+  listIngredients.append(li)
+})
+console.log(ingredients)
+
+
