632031d9632d6cae48f18253bcd5a3f77c04a590
Anatolii <anatoly.tomnyak@gmail.com>
2022-07-11 23:45:37 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+console.log('decrementBtn', decrementBtn);
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+console.log('incrementBtn', incrementBtn);
+
+let counterValue = 0;
+const counterRef = document.querySelector('#value');
+
+console.log('counterRef', counterRef);
+
+
+decrementBtn.addEventListener('click', ()=> {
+    counterValue += Number(decrementBtn.textContent);
+    counterRef.innerHTML = counterValue;
+});
+
+incrementBtn.addEventListener('click', ()=> {
+    counterValue += Number(incrementBtn.textContent);
+    counterRef.innerHTML = counterValue;
+});
+
