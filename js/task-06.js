120b555decb8a48360fc9ebfa9b5990e30c95b2a
Anatolii <anatoly.tomnyak@gmail.com>
2022-07-12 11:34:23 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,18 @@
 const textInput = document.querySelector('#validation-input');
 let validLength = parseInt(textInput.getAttribute('data-length'));
 
-textInput.addEventListener('blur', (event) => {
+
+const focusInput = (event) => {
     if (event.currentTarget.value.trim().length === validLength) {
-        event.currentTarget.classList.add('invalid');
-        event.currentTarget.classList.remove('valid');
+        event.currentTarget.classList.add('valid');
+        event.currentTarget.classList.remove('invalid');
     } else {
         event.currentTarget.classList.add('invalid');
         event.currentTarget.classList.remove('valid');
     }
-})
+    console.log(focusInput)
+}
+
+textInput.addEventListener('blur', focusInput)
 
 console.log(textInput)
