3e5fa05e0299f9381171e89245a9050db9be77cd
Anatolii <anatoly.tomnyak@gmail.com>
2022-07-12 20:39:42 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formToLogin = document.querySelector('.login-form');
+
+formToLogin.addEventListener('submit', formSubmit)
+
+function formSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+    
+}
+
 
